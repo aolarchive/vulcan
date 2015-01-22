3c4a38191a869cb7ea43a6bb4f5c80ece95a4273
jnuche <jaime.nuche@adtech.com>
2015-01-22 09:38:35 +0000

    Version bumped. Fixed typo

--- src/main/java/com/aol/advertising/dmp/disruptor/api/DisruptorAvroFileWriterBuilder.java
+++ src/main/java/com/aol/advertising/dmp/disruptor/api/DisruptorAvroFileWriterBuilder.java
@@ -66,7 +66,7 @@ public class DisruptorAvroFileWriterBuilder implements Steps {
   }
 
   private void useSensibleDefaults() {
-    ringBufferSize = 2048;;
+    ringBufferSize = 2048;
     producerType = ProducerType.MULTI;
     waitStrategy = new SleepingWaitStrategy();
   }
